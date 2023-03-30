fac86f02a0bbfc2a0a43c62b2c292445086a9d79
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-03-30 17:57:31 +0200

    😀 set up icons / svg's

---
+++ library/icons.tsx
@@ -0,0 +1,30 @@
+BsTwitter
+
+import {
+  BsTwitter,
+  BsFillBellFill,
+  BsPersonFill,
+  BsFillSunFill,
+  BsMoonFill,
+  BsDot,
+} from 'react-icons/bs'
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
+import { RiHome7Fill } from 'react-icons/ri'
+import { BiLogOut } from 'react-icons/bi'
+import { SlBubble } from 'react-icons/sl'
+import { FaCalendarAlt } from 'react-icons/fa'
+
+export {
+  BsTwitter as Twitter,
+  BsFillBellFill as Bell,
+  BsPersonFill as Profile,
+  BsFillSunFill as Sun,
+  BsMoonFill as Moon,
+  RiHome7Fill as Home,
+  BiLogOut as LogOut,
+  AiOutlineHeart as OutlinedHeart,
+  AiFillHeart as FilledHeart,
+  SlBubble as Comment,
+  FaCalendarAlt as Calendar,
+  BsDot as Dot,
+}
